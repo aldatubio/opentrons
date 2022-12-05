a3dd2e36d59a51ecde5a204143a81a35817eaaef
lucy-aldatubio <119699492+lucy-aldatubio@users.noreply.github.com>
2022-12-05 10:58:14 -0500

    Extended metadata description in app view

--- 2022-11-17_8x8primerScreen.py
+++ 2022-11-17_8x8primerScreen.py
@@ -49,7 +49,11 @@ metadata = {
     'apiLevel': '2.13',
     'protocolName': '8x8 Primer Screen',
     'author': 'OP13 LL',
-    'description': 'Test 64 different forward-reverse primer pairs for sensitivity (ability to amplify target template) and specificity (inability to amplify NTC).'
+    'description': '''Test 64 different forward-reverse primer pairs for sensitivity (ability to amplify target template) and specificity (inability to amplify NTC). ||
+                       LIQUID SETUP: ||
+                       1.5mL snap cap tubes: tubes A1-4, B1-4 = 13X forward primers (80µL each), tubes C1-4, D1-4 = 13X reverse primers (80 µL each). ||
+                       5mL screw cap tubes: tube A5 = master mix with target template (3.6mL), tube C5 = negative control master mix (3.6mL).
+    '''
 }
 
 def run(protocol: protocol_api.ProtocolContext):
