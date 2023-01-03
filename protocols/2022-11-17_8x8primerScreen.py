ec62cc06f615fd00228b0df7829fbdf5a1d1e2f3
Lucy OP13 <lucy@aldatubio.com>
2023-01-03 11:04:25 -0500

    Updated reagent volumes, switched to 15mL tubes

--- protocols/2022-11-17_8x8primerScreen.py
+++ protocols/2022-11-17_8x8primerScreen.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # # 8x8 Primer Screen
-# **Updated 2022-12-12** <br>
+# **Updated 2023-01-03** <br>
 # **Author: OP13 LL**
 # 
 # **Purpose:** Test 64 different forward-reverse primer pairs for sensitivity (ability to amplify target template) and specificity (inability to amplify NTC). <br>
@@ -29,14 +29,14 @@
 #  - 17µL master mix
 #  
 # **Deck setup:**
-#  - **1:** 15-count 5mL screw cap tube rack, with tubes as follows:
-#      - tube A5: master mix with target template (3.6mL)
-#      - tube C5: negative control master mix (3.6mL)
+#  - **1:** 15-count 15mL screw cap tube rack, with tubes as follows:
+#      - tube A5: master mix with target template (6.76mL)
+#      - tube C5: negative control master mix (6.93mL)
 #  - **2:** Applied Biosystems 384-well MicroAmp plate
 #  - **4:** 96-count 300µL tip rack (protocol uses 2 tips)
 #  - **5:** 24-count 1.5mL snap cap tube rack, with tubes as follows:
-#      - tubes A1-4, B1-4: 13X forward primers (100µL each)
-#      - tubes C1-4, D1-4: 13X reverse primers (100µL each)
+#      - tubes A1-4, B1-4: 13X forward primers (~250µL each)
+#      - tubes C1-4, D1-4: 13X reverse primers (~250µL each)
 #  - **6:** 96-count 20µL tip rack (protocol uses 16 tips)
 # 
 # 
@@ -50,8 +50,8 @@ metadata = {
     'protocolName': '8x8 Primer Screen (updated 2022-12-05)',
     'author': 'OP13 LL',
     'description': '''LIQUID SETUP:
-                        1.5mL tubes: tubes A1-4, B1-4 = forward primers (100µL each); tubes C1-4, D1-4 = reverse primers (100µL each).
-                        5mL tubes: tube A5 = master mix with target template (3.6mL), tube C5 = negative control master mix (3.6mL).
+                        1.5mL tubes: tubes A1-4, B1-4 = forward primers (~250µL each); tubes C1-4, D1-4 = reverse primers (~250µL each).
+                        15mL tubes: tube A5 = master mix with target template (6.76mL), tube C5 = negative control master mix (6.93mL).
                         '''
 }
 
@@ -91,7 +91,12 @@ def run(protocol: protocol_api.ProtocolContext):
         labware_def = json.load(labware_file)
         mastermixes = protocol.load_labware_from_definition(labware_def, 1)
     '''
-    mastermixes = protocol.load_labware('usascientific_15_tuberack_5000ul', 1)
+    # to use 5mL tubes, use our lab's custom definition:
+    # mastermixes = protocol.load_labware('usascientific_15_tuberack_5000ul', 1)
+    
+    # Opentrons doesn't have a labware definition for the Nunc / ThermoFisher 15mL tubes we use;
+    # however, NEST tube technical specs are very close to the Nunc tubes, so we'll use this definition
+    mastermixes = protocol.load_labware('opentrons_15_tuberack_nest_15ml_conical', 1)
 
     # pipette initialization/setup
     p20 = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=[p20tips])
