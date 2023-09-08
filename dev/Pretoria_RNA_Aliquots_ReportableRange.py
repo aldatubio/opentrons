b1316372ddf1d93f97675409068f22c45dbe59ab
Lucy OP13 <lucy@aldatubio.com>
2023-09-08 15:54:44 -0400

    ci: blowing out in source well for all

--- dev/Pretoria_RNA_Aliquots_ReportableRange.py
+++ dev/Pretoria_RNA_Aliquots_ReportableRange.py
@@ -158,7 +158,9 @@ def run(protocol: protocol_api.ProtocolContext):
      p300.distribute(
          aliquot_vol,
          [tubes.wells()[dil]],
-         [plate.wells()[well] for well in wells_to_fill]
+         [plate.wells()[well] for well in wells_to_fill],
+         blow_out = True,
+         blowout_location = 'source well'
       )
 
 
@@ -212,7 +214,9 @@ def run(protocol: protocol_api.ProtocolContext):
      p300.distribute(
          aliquot_vol,
          [tubes.wells()[dil]],
-         [plate.wells()[well] for well in wells_to_fill]
+         [plate.wells()[well] for well in wells_to_fill],
+         blow_out = True,
+         blowout_location = 'source well'
       )
 
 
@@ -237,7 +241,9 @@ def run(protocol: protocol_api.ProtocolContext):
     p300.distribute(
         aliquot_vol,
         tubes[negatives_location],
-        [plate.wells()[well] for well in wells_to_fill]
+        [plate.wells()[well] for well in wells_to_fill],
+        blow_out = True,
+        blowout_location = 'source well'
       )
     
   protocol.home()
