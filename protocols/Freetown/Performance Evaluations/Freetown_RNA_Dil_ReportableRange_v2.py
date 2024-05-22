9329eb6081883177b8f217d87b39f36faa862b60
Lucy OP13 <lucy@aldatubio.com>
2024-05-22 16:32:40 -0400

    dev: using csv input for dil volumes

--- protocols/Freetown/Performance Evaluations/Freetown_RNA_Dil_ReportableRange_v2.py
+++ protocols/Freetown/Performance Evaluations/Freetown_RNA_Dil_ReportableRange_v2.py
@@ -1,10 +1,12 @@
 '''
 Project Freetown
 RNA Dilutions for Reportable Range
-Updated 2024-05-21
+Updated 2024-05-22
 Author: OP13 LL
 
 Adapted from Project Pretoria's RNA Dilutions for Reportable Range protocol
+
+Basic design: takes parameters (volumes, number of tubes/dilutions) as a pasted csv table
 
 ----------------------------
   Stock Conc.    2.5E+6 µL  
@@ -60,10 +62,39 @@ Empty 1.5mL tubes: B1-D1, A2-D2, A3-D3, and A4-B4 [first 4 columns] of 24-ct 1.5
 
 '''
 
+import csv
+import io
 from opentrons import protocol_api
 
+
+###
+### Paste your raw csv data here
+###
+csv_raw = '''1,17,68
+2,17,68
+3,17,68
+4,17,68
+5,17,68
+6,17,68
+7,36,144
+8,130,130
+9,130,130
+10,130,130
+11,130,130
+12,130,130
+13,130,130
+'''
+###
+###
+###
+
+
+# Parsing csv input - StringIO method treats pasted string as file object
+csv_file = io.StringIO(csv_raw)
+csv_reader = csv.reader(csv_file, delimiter = ",")
+
 metadata = {
-    'apiLevel': '2.14',
+    'apiLevel': '2.13',
     'protocolName': 'Freetown | RNA Dilutions for Reportable Range',
     'author': 'OP13 LL',
     'description': '''Performs fourteen-point dilution series. 
@@ -75,29 +106,10 @@ def run(protocol: protocol_api.ProtocolContext):
     protocol.home()
 
     ###
-    ### User-defined variables
-    ###
-
-    '''
-    # Original Cairo setup:
-
-    diluent_vol = [200, 300, 300]
-    diluent_location = 'B1'
-
-    RNA_vol = [50, 75, 300]
-    num_tubes = [7, 1, 6] # number of tubes to contain RNA; arranged in columns (A1, B1, C1, D1, A2...)
-    '''
-
-    diluent_vol = [68, 144, 130]
-    diluent_location = 'B1'
-    
-    RNA_vol = [17, 36, 130]
-    num_tubes = [7, 1, 6] # number of tubes to contain RNA; arranged in columns (A1, B1, C1, D1, A2...)
-    
-    
-    ###
     ### Initialization
     ###
+
+    diluent_location = 'B1'
 
     p300tips = protocol.load_labware('opentrons_96_filtertiprack_200ul', 3)
     p20tips = protocol.load_labware('opentrons_96_filtertiprack_20ul', 6)
@@ -110,10 +122,12 @@ def run(protocol: protocol_api.ProtocolContext):
     p20 = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[p20tips])
 
 
-    ###
+    ### needs edits
+
     ### Visualization of deck layout - API 2.14 and above only!
     ### To use protocol simulator, downgrade this protocol to 2.13 and comment out this section
     ###
+    '''
     # ************************************
     diluent_viz = protocol.define_liquid(
         'Diluent',
@@ -149,47 +163,43 @@ def run(protocol: protocol_api.ProtocolContext):
             0
         )
     # ************************************
-
+    '''
 
     ###
     ### 1. Transfer diluent
     ###
 
     p300.pick_up_tip()
-    tube_counter = 1
 
-    for j in num_tubes:
-
-        for i in range(tube_counter, j + tube_counter):
-            if tube_counter >= sum(num_tubes):
-                break
-            p300.transfer(
-                diluent_vol[j], ###need to check whether this will work - I am doubtful
-                diluent[diluent_location],
-                [tubes.wells()[i]],
-                new_tip = 'never'
-            )
-            tube_counter += 1
+    for row in csv_reader:
+        p300.transfer(
+            int(row[2]),
+            diluent[diluent_location],
+            [tubes.wells()[int(row[0])]],
+            new_tip = 'never'
+        )
 
     p300.drop_tip()
 
-## Below content still needs amendment
 
     ###
     ### 2. Transfer RNA
     ###
 
-    for i in range(1, num_tubes):
-
-        p1000.transfer(
-            RNA_vol,
-            [tubes.wells()[i - 1]],
-            [tubes.wells()[i]],
+    for row in csv_reader:
+        if (int(row[1]) + int(row[2]))*0.8 < 200:
+            mix_vol = (int(row[1]) + int(row[2]))*0.8
+        else:
+            mix_vol = 200
+        
+        p300.transfer(
+            int(row[1]),
+            [tubes.wells()[int(row[0]) - 1]],
+            [tubes.wells()[int(row[0])]],
             mix_after = (
                 5,
-                (diluent_vol + RNA_vol)*0.8
+                mix_vol
             )
         )
-
 
     protocol.home()
