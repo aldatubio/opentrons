7f3a21a0e57d08f8578b3d2ea496894badb94b31
Lucy OP13 <lucy@aldatubio.com>
2024-05-24 16:33:35 -0400

    dev: mastermix plating for RR

---
+++ protocols/Freetown/Performance Evaluations/Freetown_Mastermix_Plating_96well.py
@@ -0,0 +1,60 @@
+# Project Freetown
+# Reportable Range - Mastermix Plating
+# Updated 2024-05-24
+# Author: OP13 LL
+
+from opentrons import protocol_api
+
+metadata = {
+    'apiLevel': '2.13',
+    'protocolName': 'Freetown | Mastermix Plating for Reportable Range',
+    'author': 'OP13 LL',
+    'description': '''Plates master mix for reportable range experiments [all wells of a 96-well plate]. | 
+                        Place completed 2x mastermix in slot A1 of a tube rack [1.5mL tube for 1 plate, 5mL tube for 2+ plates].'''
+}
+
+def run(protocol: protocol_api.ProtocolContext):
+
+    # 0. Initialization
+    
+    number_of_plates = 2
+    volume = 10
+    
+    protocol.home()
+
+    p300tips = protocol.load_labware('opentrons_96_filtertiprack_200ul', 6, 'p300 Tips')
+    
+    plateDict = {}
+    for i in range(number_of_plates):
+        plateDict[str(i+1)] = protocol.load_labware('thermo_96_well_endura_0.1ml', i+1, 'Plate '+str(i+1))
+
+    if number_of_plates > 1:
+        rack = protocol.load_labware('usascientific_15_tuberack_5000ul', 5, 'MM: Plates > 1')
+    else:
+        rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 5, 'MM: Plates = 1')
+
+    p300 = protocol.load_instrument('p300_single_gen2', 'right', tip_racks=[p300tips])
+
+
+
+    # 1. Adding mastermix to all wells
+
+    list = []
+
+    for i in range(96):
+        list.append(i)
+
+    #ARC_wells = [num for num in list if (num % 8 != 2) or (num < 64)]  # remove wells C9-C12 from list: #66, 74, 82, and 90
+
+    for i in range(number_of_plates):
+
+        p300.distribute(
+            volume,
+            rack['A1'],
+            [plateDict[str(i+1)].wells()[wellIndex] for wellIndex in list],
+            disposal_volume = 10,
+            blow_out = True,
+            blowout_location = "source well"
+        )
+
+    protocol.home()
