8621db458a1f124e1d012638fec389929c17a778
Lucy OP13 <lucy@aldatubio.com>
2025-01-13 16:55:08 -0500

    fix: param names still too long

--- protocols/Freetown/Assay Development/PrimerOptimization_Custom.py
+++ protocols/Freetown/Assay Development/PrimerOptimization_Custom.py
@@ -67,8 +67,8 @@ def add_parameters(parameters: protocol_api.Parameters):
         choices=[
             {'display_name': '40 µL (one plate)', 'value': 1},
             {'display_name': '80 µL (two plates)', 'value': 2},
-            {'display_name': '120 µL (three plates - 1.5 mL tubes)', 'value': 3},
-            {'display_name': '160 µL (four plates - 1.5 mL tubes)', 'value': 4}
+            {'display_name': '120 µL (three plates)', 'value': 3},
+            {'display_name': '160 µL (four plates)', 'value': 4}
         ]
     )
 
